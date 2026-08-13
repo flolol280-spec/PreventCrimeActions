21c2fdc60ec7acf86a505f240a2b77231126ca6b
Flochi <flolol280@gmail.com>
2026-08-13 17:25:49 +0200

    Formating

--- PreventCrimeActions.lua
+++ PreventCrimeActions.lua
@@ -434,8 +434,8 @@ EVENT_MANAGER:RegisterForEvent("PCA", EVENT_PLAYER_COMBAT_STATE, function(_, inC
         PCA_DisableWhenInCombat() -- Disable Crime Prevention in Combat
     else
         if not PCA.preventCrime then
-        return
-    end
+            return
+        end
         PCA_EnableWhenOutOfCombat() -- Enable Crime Prevention outside of Combat    
 
         if IsPlayerInWerewolfForm() then
